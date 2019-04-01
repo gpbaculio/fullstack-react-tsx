5d808a0856714f7b0e66fcd7337ae6db7d66a34d
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-01 16:45:57 +0800

    remove build ignore

---
+++ frontend-ts/build/precache-manifest.163d51e8d52238dff24fdfc6fc3b8e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "165eda13e0e5cd0d44ae",
+    "url": "/static/js/main.4ad06166.chunk.js"
+  },
+  {
+    "revision": "63fbdbfc4988441c32d4",
+    "url": "/static/js/2.4fd904d8.chunk.js"
+  },
+  {
+    "revision": "165eda13e0e5cd0d44ae",
+    "url": "/static/css/main.9e6053a7.chunk.css"
+  },
+  {
+    "revision": "63fbdbfc4988441c32d4",
+    "url": "/static/css/2.69e1b912.chunk.css"
+  },
+  {
+    "revision": "9521062cd6c4f6c029365ed8d5d3f73d",
+    "url": "/index.html"
+  }
+];
