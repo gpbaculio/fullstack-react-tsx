ff55f4bedef97f8ef34fbc8689daaaa060cf01b3
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-01 19:48:17 +0800

    build

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -26,7 +26,7 @@ class App {
             this.app.use(express.static(path.join(__dirname, '../../frontend/build')));
             // Handle React routing, return all requests to React app
             this.app.get('*', (req, res) => {
-                res.sendFile(path.join(__dirname, '../../', 'frontend/build', 'index.html'));
+                res.sendFile(path.join(__dirname + '../../frontend/build/index.html'));
             });
         }
     }
--- backend/lib/app.ts
+++ backend/lib/app.ts
@@ -29,9 +29,7 @@ class App {
       );
       // Handle React routing, return all requests to React app
       this.app.get('*', (req, res) => {
-        res.sendFile(
-          path.join(__dirname, '../../', 'frontend/build', 'index.html')
-        );
+        res.sendFile(path.join(__dirname+'../../frontend/build/index.html'));
       });
     }
   }
