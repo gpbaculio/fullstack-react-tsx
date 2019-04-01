672597d14a6389894a82f4bd94cc0ca18ec8df10
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-02 00:50:20 +0800

    change path

--- backend/lib/app.ts
+++ backend/lib/app.ts
@@ -29,10 +29,10 @@ class App {
     if (process.env.NODE_ENV === 'production') {
       // Serve any static files
       console.log('prod!');
-      this.app.use(express.static(path.join('../../frontend/build')));
+      this.app.use(express.static('../../frontend/build'));
       // Handle React routing, return all requests to React app
       this.app.get('/*', (req, res) => {
-        res.sendFile(path.join('../../frontend/build', 'index.html'));
+        res.sendFile('../../frontend/build/index.html');
       });
     }
   }
