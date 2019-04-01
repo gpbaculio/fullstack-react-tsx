5bf68fee35ace8ca1cd4fb4f569ed3a4f02836d6
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-01 16:43:52 +0800

    fix build

--- backend-ts/lib/app.ts
+++ backend-ts/lib/app.ts
@@ -22,7 +22,6 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
     // serving static files
-    this.app.use(express.static('public'));
     if (process.env.NODE_ENV === 'production') {
       // Serve any static files
       this.app.use(
